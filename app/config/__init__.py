11081067faaaf94982b7b88dec452a496a032177
Dries007 <admin@dries007.net>
2018-02-05 23:41:34 +0100

    minor fixes + google analytics

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,10 +1,11 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://%s:%s@db/%s' % (os.environ['POSTGRES_USER'], os.environ['POSTGRES_PASSWORD'], os.environ['POSTGRES_DB'])
+SQLALCHEMY_DATABASE_URI = 'postgresql://%s:%s@localhost/%s' % (os.environ['POSTGRES_USER'], os.environ['POSTGRES_PASSWORD'], os.environ['POSTGRES_DB'])
 SECRET_KEY = os.environ['FLASK_SECRET']
 
 TEMPLATES_AUTO_RELOAD = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+GOOGLE_ANALYTICS = os.environ['GOOGLE_ANALYTICS'] or None
 
 MAIL_SERVER = os.environ['MAIL_SERVER']
 MAIL_USERNAME = os.environ['MAIL_USERNAME']
@@ -20,3 +21,4 @@ with open(os.path.dirname(__file__) + '/activation_email.md') as f:
 
 with open(os.path.dirname(__file__) + '/reset_email.md') as f:
     MAIL_TEMPLATE_RESET = f.read()
+
